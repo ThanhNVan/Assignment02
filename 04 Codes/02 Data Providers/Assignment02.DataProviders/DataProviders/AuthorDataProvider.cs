6308cec65fc638677a68f38ac1eda0fda7e3f1bc
ThanhNVan <thanh.van@wave5.nl>
2023-08-08 11:12:52 +0700

    added demo data

--- 04 Codes/02 Data Providers/Assignment02.DataProviders/DataProviders/AuthorDataProvider.cs
+++ 04 Codes/02 Data Providers/Assignment02.DataProviders/DataProviders/AuthorDataProvider.cs
@@ -12,24 +12,4 @@ public class AuthorDataProvider : BaseEntityDataProvider<Author, AppDbContext>, 
                                 IDbContextFactory<AppDbContext> dbContextFactory) : base(logger, dbContextFactory) {
     }
     #endregion
-
-    public async Task TestTransaction() {
-        var context = base.GetContext();    
-        var stragtegy = this.GetContext().Database.CreateExecutionStrategy();
-
-        await stragtegy.ExecuteAsync( async () => {
-            using (var transaction = await context.Database.BeginTransactionAsync()){
-                try {
-                    // save something here
-                    // AsNoTracking()
-                    await context.SaveChangesAsync();
-                    await transaction.CommitAsync();
-                } catch (Exception ex) {
-                    this._logger.LogError(ex.Message);
-                    await transaction.RollbackAsync();
-                    throw;
-                }
-            }
-        });
-    }
 }
--- 04 Codes/04 WebApi Providers/Assignment02.WebApiProviders/Controllers/AddDemoDataController.cs
+++ 04 Codes/04 WebApi Providers/Assignment02.WebApiProviders/Controllers/AddDemoDataController.cs
@@ -1,15 +1,16 @@
 ﻿using Assignment02.DataProviders;
 using Assignment02.EntityProviders;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Assignment02.WebApiProviders;
 
-[ApiController]
-[Route("Api/V1/[controller]")]
+//[ApiController]
+//[Route("Api/V1/[controller]")]
 public class AddDemoDataController : ControllerBase
 {
 	#region [ Fields ]
-	private readonly AppDbContext _dbContext;
+	private readonly IDbContextFactory<AppDbContext> _dbContext;
 	#endregion
 
 	#region [ Properties ]
@@ -22,8 +23,8 @@ public class AddDemoDataController : ControllerBase
 	#endregion
 
 	#region [ CTor ]
-	public AddDemoDataController(AppDbContext dataContext) {
-		_dbContext = dataContext;	
+	public AddDemoDataController(IDbContextFactory<AppDbContext> dbContext) {
+        _dbContext = dbContext;	
 		AuthorList = new List<Author>();
 		BookAuthorList = new List<BookAuthor>();
 		BookList = new List<Book>();
@@ -33,24 +34,52 @@ public class AddDemoDataController : ControllerBase
 
 		LoadAddData();
     }
-    #endregion
+	#endregion
 
-    #region [ Public Methods - AddDemoDataAsync ]
-    [HttpGet]
+	#region [ Public Methods - AddDemoDataAsync ]
+	[HttpGet]
 	public async Task<IActionResult> AddDemoDataAsync() {
 		try {
 
-		} catch (Exception ex) {
+			var dbContext = await this._dbContext.CreateDbContextAsync();
+			var strategy = dbContext.Database.CreateExecutionStrategy();
+			await strategy.ExecuteAsync( async () => {
+				using var transaction = await dbContext.Database.BeginTransactionAsync();
+				try {
+					await dbContext.AddRangeAsync(this.PublisherList);
+					await dbContext.AddRangeAsync(this.BookList);
+					await dbContext.AddRangeAsync(this.AuthorList);
+					await dbContext.AddRangeAsync(this.BookAuthorList);
+					await dbContext.AddRangeAsync(this.RoleList);
+					await dbContext.AddRangeAsync(this.UserList);
+                    await dbContext.SaveChangesAsync();
+                    await transaction.CommitAsync();
+					
+                } catch (Exception ex) {
+					var aa = ex.Message;
+                    await transaction.RollbackAsync();
+                    throw;
+				}
+            });
+                return Ok();
 
-			throw;
+		} catch (Exception ex) {
+            return BadRequest();
+            throw;
 		}
 	}
 	#endregion
 
 	#region [ Private Methods - Seed Data ]
 	private void LoadAddData() {
-		LoadAuthor();
-		LoadBook();
+		this.LoadPublisher();
+
+		this.LoadBook();
+        this.LoadAuthor();
+		this.LoadBookAuthor();
+		this.LoadRole();
+
+		this.LoadUser();
     }
 
 	private void LoadAuthor() {
@@ -125,7 +154,7 @@ public class AddDemoDataController : ControllerBase
 										sales: 0,
 										notes: "Book note 0",
 										publishDate: DateTime.UtcNow.AddDays(0),
-										publisherId: "Book publisherId 0"); 
+										publisherId: this.PublisherList[0].Id); 
 		
 		var book_1 = Factory.CreateBook(title: "Book title 1",
 										type: "Book type 1",
@@ -135,7 +164,7 @@ public class AddDemoDataController : ControllerBase
 										sales: 1,
 										notes: "Book note 1",
 										publishDate: DateTime.UtcNow.AddDays(1),
-										publisherId: "Book publisherId 1"); 
+										publisherId: this.PublisherList[0].Id); 
 		
 		var book_2 = Factory.CreateBook(title: "Book title 2",
 										type: "Book type 2",
@@ -145,7 +174,7 @@ public class AddDemoDataController : ControllerBase
 										sales: 2,
 										notes: "Book note 2",
 										publishDate: DateTime.UtcNow.AddDays(2),
-										publisherId: "Book publisherId 2"); 
+										publisherId: this.PublisherList[1].Id); 
 		
 		var book_3 = Factory.CreateBook(title: "Book title 3",
 										type: "Book type 3",
@@ -155,7 +184,7 @@ public class AddDemoDataController : ControllerBase
 										sales: 3,
 										notes: "Book note 3",
 										publishDate: DateTime.UtcNow.AddDays(3),
-										publisherId: "Book publisherId 3"); 
+										publisherId: this.PublisherList[1].Id); 
 		
 		var book_4 = Factory.CreateBook(title: "Book title 4",
 										type: "Book type 4",
@@ -165,7 +194,7 @@ public class AddDemoDataController : ControllerBase
 										sales: 4,
 										notes: "Book note 4",
 										publishDate: DateTime.UtcNow.AddDays(4),
-										publisherId: "Book publisherId 4"); 
+										publisherId: this.PublisherList[2].Id); 
 		
 		var book_5 = Factory.CreateBook(title: "Book title 5",
 										type: "Book type 5",
@@ -175,7 +204,7 @@ public class AddDemoDataController : ControllerBase
 										sales: 5,
 										notes: "Book note 5",
 										publishDate: DateTime.UtcNow.AddDays(5),
-										publisherId: "Book publisherId 5"); 
+										publisherId: this.PublisherList[2].Id); 
 		
 		var book_6 = Factory.CreateBook(title: "Book title 6",
 										type: "Book type 6",
@@ -185,7 +214,7 @@ public class AddDemoDataController : ControllerBase
 										sales: 6,
 										notes: "Book note 6",
 										publishDate: DateTime.UtcNow.AddDays(6),
-										publisherId: "Book publisherId 6"); 
+										publisherId: this.PublisherList[3].Id); 
 		
 		var book_7 = Factory.CreateBook(title: "Book title 7",
 										type: "Book type 7",
@@ -195,7 +224,7 @@ public class AddDemoDataController : ControllerBase
 										sales: 7,
 										notes: "Book note 7",
 										publishDate: DateTime.UtcNow.AddDays(7),
-										publisherId: "Book publisherId 7"); 
+										publisherId: this.PublisherList[3].Id); 
 		
 		var book_8 = Factory.CreateBook(title: "Book title 8",
 										type: "Book type 8",
@@ -205,7 +234,7 @@ public class AddDemoDataController : ControllerBase
 										sales: 8,
 										notes: "Book note 8",
 										publishDate: DateTime.UtcNow.AddDays(8),
-										publisherId: "Book publisherId 8"); 
+										publisherId: this.PublisherList[2].Id); 
 		
 		var book_9 = Factory.CreateBook(title: "Book title 9",
 										type: "Book type 9",
@@ -215,7 +244,7 @@ public class AddDemoDataController : ControllerBase
 										sales: 9,
 										notes: "Book note 9",
 										publishDate: DateTime.UtcNow.AddDays(9),
-										publisherId: "Book publisherId 9"); 
+										publisherId: this.PublisherList[3].Id); 
 		
 		var book_10 = Factory.CreateBook(title: "Book title 10",
 										type: "Book type 10",
@@ -225,7 +254,7 @@ public class AddDemoDataController : ControllerBase
 										sales: 10,
 										notes: "Book note 10",
 										publishDate: DateTime.UtcNow.AddDays(10),
-										publisherId: "Book publisherId 10"); 
+										publisherId: this.PublisherList[1].Id); 
 
 		BookList.Add(book_0);
 		BookList.Add(book_1);
@@ -239,5 +268,279 @@ public class AddDemoDataController : ControllerBase
 		BookList.Add(book_9);
 		BookList.Add(book_10);
 	}
-	#endregion
+
+	private void LoadPublisher() {
+		var publisher_0 = Factory.CreatePublisher(name: "Publisher name 0",
+													address: "Publisher address 0",
+													city: "District 3",
+													state: "Ho CHi Minh city",
+													country: "Vietnam");
+		
+		var publisher_1 = Factory.CreatePublisher(name: "Publisher name 1",
+													address: "Publisher address 1",
+													city: "District 9",
+													state: "Ho CHi Minh city",
+													country: "Vietnam");
+		
+		var publisher_2 = Factory.CreatePublisher(name: "Publisher name 2",
+													address: "Publisher address 2",
+													city: "District 10",
+													state: "Ho CHi Minh city",
+													country: "Vietnam");
+		
+		var publisher_3 = Factory.CreatePublisher(name: "Publisher name 3",
+													address: "Publisher address 3",
+													city: "Binh Thanh District",
+													state: "Ho CHi Minh city",
+													country: "Vietnam");
+		this.PublisherList.Add(publisher_0);
+		this.PublisherList.Add(publisher_1);
+		this.PublisherList.Add(publisher_2);
+		this.PublisherList.Add(publisher_3);
+	}
+
+	private void LoadBookAuthor() {
+		var bookAuthor_0 = Factory.CreateBookAuthor(authorId: this.AuthorList[0].Id,
+													bookId: this.BookList[0].Id,
+													authorOrder: "BookAuthor author order 0",
+													royalityPercentage: "BookAuthor royality percentage");
+
+		var bookAuthor_1 = Factory.CreateBookAuthor(authorId: this.AuthorList[0].Id,
+													bookId: this.BookList[1].Id,
+													authorOrder: "BookAuthor author order 1",
+													royalityPercentage: "BookAuthor royality percentage 1");
+
+		var bookAuthor_2 = Factory.CreateBookAuthor(authorId: this.AuthorList[0].Id,
+													bookId: this.BookList[2].Id,
+													authorOrder: "BookAuthor author order 2",
+													royalityPercentage: "BookAuthor royality percentage 2");
+		
+		var bookAuthor_3 = Factory.CreateBookAuthor(authorId: this.AuthorList[1].Id,
+													bookId: this.BookList[2].Id,
+													authorOrder: "BookAuthor author order 3",
+													royalityPercentage: "BookAuthor royality percentage 3");
+
+
+		var bookAuthor_4 = Factory.CreateBookAuthor(authorId: this.AuthorList[3].Id,
+													bookId: this.BookList[2].Id,
+													authorOrder: "BookAuthor author order 4",
+													royalityPercentage: "BookAuthor royality percentage 4");
+
+		var bookAuthor_5 = Factory.CreateBookAuthor(authorId: this.AuthorList[3].Id,
+													bookId: this.BookList[3].Id,
+													authorOrder: "BookAuthor author order 5",
+													royalityPercentage: "BookAuthor royality percentage 5");
+
+		var bookAuthor_6 = Factory.CreateBookAuthor(authorId: this.AuthorList[3].Id,
+													bookId: this.BookList[4].Id,
+													authorOrder: "BookAuthor author order 6",
+													royalityPercentage: "BookAuthor royality percentage 6");
+
+		var bookAuthor_7 = Factory.CreateBookAuthor(authorId: this.AuthorList[4].Id,
+													bookId: this.BookList[5].Id,
+													authorOrder: "BookAuthor author order 7",
+													royalityPercentage: "BookAuthor royality percentage 7");
+
+		var bookAuthor_8 = Factory.CreateBookAuthor(authorId: this.AuthorList[1].Id,
+													bookId: this.BookList[5].Id,
+													authorOrder: "BookAuthor author order 8",
+													royalityPercentage: "BookAuthor royality percentage 8");
+		
+		var bookAuthor_9 = Factory.CreateBookAuthor(authorId: this.AuthorList[5].Id,
+													bookId: this.BookList[6].Id,
+													authorOrder: "BookAuthor author order 9",
+													royalityPercentage: "BookAuthor royality percentage 9");
+
+		var bookAuthor_10 = Factory.CreateBookAuthor(authorId: this.AuthorList[1].Id,
+													bookId: this.BookList[6].Id,
+													authorOrder: "BookAuthor author order 10",
+													royalityPercentage: "BookAuthor royality percentage 10");
+		
+		var bookAuthor_11 = Factory.CreateBookAuthor(authorId: this.AuthorList[0].Id,
+													bookId: this.BookList[7].Id,
+													authorOrder: "BookAuthor author order 11",
+													royalityPercentage: "BookAuthor royality percentage 11");
+
+		var bookAuthor_12 = Factory.CreateBookAuthor(authorId: this.AuthorList[2].Id,
+													bookId: this.BookList[6].Id,
+													authorOrder: "BookAuthor author order 12",
+													royalityPercentage: "BookAuthor royality percentage 12");
+		
+		var bookAuthor_13 = Factory.CreateBookAuthor(authorId: this.AuthorList[3].Id,
+													bookId: this.BookList[7].Id,
+													authorOrder: "BookAuthor author order 13",
+													royalityPercentage: "BookAuthor royality percentage 13");
+
+
+		var bookAuthor_14 = Factory.CreateBookAuthor(authorId: this.AuthorList[4].Id,
+													bookId: this.BookList[8].Id,
+													authorOrder: "BookAuthor author order 14",
+													royalityPercentage: "BookAuthor royality percentage 14");
+	
+		var bookAuthor_15 = Factory.CreateBookAuthor(authorId: this.AuthorList[5].Id,
+													bookId: this.BookList[9].Id,
+													authorOrder: "BookAuthor author order 15",
+													royalityPercentage: "BookAuthor royality percentage 15");
+
+		var bookAuthor_16 = Factory.CreateBookAuthor(authorId: this.AuthorList[0].Id,
+													bookId: this.BookList[10].Id,
+													authorOrder: "BookAuthor author order 16",
+													royalityPercentage: "BookAuthor royality percentage 16");
+		
+		var bookAuthor_17 = Factory.CreateBookAuthor(authorId: this.AuthorList[5].Id,
+													bookId: this.BookList[10].Id,
+													authorOrder: "BookAuthor author order 17",
+													royalityPercentage: "BookAuthor royality percentage 17");
+		this.BookAuthorList.Add(bookAuthor_0);
+		this.BookAuthorList.Add(bookAuthor_1);
+		this.BookAuthorList.Add(bookAuthor_2);
+		this.BookAuthorList.Add(bookAuthor_3);
+		this.BookAuthorList.Add(bookAuthor_4);
+		this.BookAuthorList.Add(bookAuthor_5);
+		this.BookAuthorList.Add(bookAuthor_6);
+		this.BookAuthorList.Add(bookAuthor_7);
+		this.BookAuthorList.Add(bookAuthor_8);
+		this.BookAuthorList.Add(bookAuthor_9);
+		this.BookAuthorList.Add(bookAuthor_10);
+		this.BookAuthorList.Add(bookAuthor_11);
+		this.BookAuthorList.Add(bookAuthor_12);
+		this.BookAuthorList.Add(bookAuthor_13);
+		this.BookAuthorList.Add(bookAuthor_14);
+		this.BookAuthorList.Add(bookAuthor_15);
+		this.BookAuthorList.Add(bookAuthor_16);
+		this.BookAuthorList.Add(bookAuthor_17);
+	}
+
+	private void LoadRole() {
+		var role_0 = Factory.CreateRole("Employee");
+		var role_1 = Factory.CreateRole("Manager");
+		var role_2 = Factory.CreateRole("Cashier");
+
+		this.RoleList.Add(role_0);
+		this.RoleList.Add(role_1);
+		this.RoleList.Add(role_2);
+	}
+
+	private void LoadUser() {
+		var user_0 = Factory.CreateUser(email: "Thanh.van0@gmail.com",
+										password: "123456",
+										firstName: "Thanh",
+										middleName: "Nhat",
+										lastName: "Van",
+										hiredDate: DateTime.UtcNow,
+										roleId: this.RoleList[0].Id,
+										publisherId: this.PublisherList[0].Id);
+		
+		var user_1 = Factory.CreateUser(email: "An.Tran0@gmail.com",
+										password: "123456",
+										firstName: "An",
+										middleName: "Vinh",
+										lastName: "Tran",
+										hiredDate: DateTime.UtcNow,
+										roleId: this.RoleList[2].Id,
+										publisherId: this.PublisherList[0].Id);
+		
+		var user_2 = Factory.CreateUser(email: "Andy.Tran0@gmail.com",
+										password: "123456",
+										firstName: "Andy",
+										middleName: "Vinh",
+										lastName: "Tran",
+										hiredDate: DateTime.UtcNow,
+										roleId: this.RoleList[1].Id,
+										publisherId: this.PublisherList[0].Id);
+		
+		var user_3 = Factory.CreateUser(email: "Thanh.van1@gmail.com",
+										password: "123456",
+										firstName: "Thanh",
+										middleName: "Nhat",
+										lastName: "Van",
+										hiredDate: DateTime.UtcNow,
+										roleId: this.RoleList[0].Id,
+										publisherId: this.PublisherList[1].Id);
+		
+		var user_4 = Factory.CreateUser(email: "An.Tran1@gmail.com",
+										password: "123456",
+										firstName: "An",
+										middleName: "Vinh",
+										lastName: "Tran",
+										hiredDate: DateTime.UtcNow,
+										roleId: this.RoleList[2].Id,
+										publisherId: this.PublisherList[1].Id);
+		
+		var user_5 = Factory.CreateUser(email: "Andy.Tran1@gmail.com",
+										password: "123456",
+										firstName: "Andy",
+										middleName: "Vinh",
+										lastName: "Tran",
+										hiredDate: DateTime.UtcNow,
+										roleId: this.RoleList[1].Id,
+										publisherId: this.PublisherList[1].Id);
+
+		var user_6 = Factory.CreateUser(email: "Thanh.van2@gmail.com",
+										password: "123456",
+										firstName: "Thanh",
+										middleName: "Nhat",
+										lastName: "Van",
+										hiredDate: DateTime.UtcNow,
+										roleId: this.RoleList[0].Id,
+										publisherId: this.PublisherList[2].Id);
+		
+		var user_7 = Factory.CreateUser(email: "An.Tran2@gmail.com",
+										password: "123456",
+										firstName: "An",
+										middleName: "Vinh",
+										lastName: "Tran",
+										hiredDate: DateTime.UtcNow,
+										roleId: this.RoleList[2].Id,
+										publisherId: this.PublisherList[2].Id);
+		
+		var user_8 = Factory.CreateUser(email: "Andy.Tran2@gmail.com",
+										password: "123456",
+										firstName: "Andy",
+										middleName: "Vinh",
+										lastName: "Tran",
+										hiredDate: DateTime.UtcNow,
+										roleId: this.RoleList[1].Id,
+										publisherId: this.PublisherList[2].Id);
+
+		var user_9 = Factory.CreateUser(email: "Thanh.van3@gmail.com",
+										password: "123456",
+										firstName: "Thanh",
+										middleName: "Nhat",
+										lastName: "Van",
+										hiredDate: DateTime.UtcNow,
+										roleId: this.RoleList[0].Id,
+										publisherId: this.PublisherList[2].Id);
+		
+		var user_10 = Factory.CreateUser(email: "An.Tran3@gmail.com",
+										password: "123456",
+										firstName: "An",
+										middleName: "Vinh",
+										lastName: "Tran",
+										hiredDate: DateTime.UtcNow,
+										roleId: this.RoleList[2].Id,
+										publisherId: this.PublisherList[3].Id);
+		
+		var user_11 = Factory.CreateUser(email: "Andy.Tran3@gmail.com",
+										password: "123456",
+										firstName: "Andy",
+										middleName: "Vinh",
+										lastName: "Tran",
+										hiredDate: DateTime.UtcNow,
+										roleId: this.RoleList[1].Id,
+										publisherId: this.PublisherList[3].Id);
+		this.UserList.Add(user_0);
+		this.UserList.Add(user_1);
+		this.UserList.Add(user_2);
+		this.UserList.Add(user_3);
+		this.UserList.Add(user_4);
+		this.UserList.Add(user_5);
+		this.UserList.Add(user_6);
+		this.UserList.Add(user_7);
+		this.UserList.Add(user_8);
+		this.UserList.Add(user_9);
+		this.UserList.Add(user_10);
+		this.UserList.Add(user_11);
+	}
+    #endregion
 }
