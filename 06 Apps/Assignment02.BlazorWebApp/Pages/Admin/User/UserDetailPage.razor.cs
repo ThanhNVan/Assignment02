3eab23dd157acd766ca28040698109123c39678e
ThanhNVan <thanh.van@wave5.nl>
2023-08-13 20:00:04 +0700

    ok

---
+++ 06 Apps/Assignment02.BlazorWebApp/Pages/Admin/User/UserDetailPage.razor.cs
@@ -0,0 +1,86 @@
+﻿using Assignment02.EntityProviders;
+using Assignment02.HttpClientProviders;
+using Blazored.SessionStorage;
+using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment02.BlazorWebApp;
+
+public partial class UserDetailPage
+{
+	#region [ Properties ]
+	[Parameter]
+	public string UserId { get; set; }
+    #endregion
+
+    #region [ Properties - Inject]
+    [Inject]
+    private NavigationManager Navigation { get; set; }
+
+    [Inject]
+    private ISessionStorageService SessionStorage { get; set; }
+
+    [Inject]
+    private HttpClientContext HttpClientContext { get; set; }
+    #endregion
+
+    #region [ Properties ]
+    public User User { get; set; }
+
+    private IEnumerable<Role> Roles { get; set; }
+
+    private IEnumerable<Publisher> Publishers { get; set; }
+    private string Role { get; set; }
+    #endregion
+
+    #region [ Methods - Override ]
+    protected override async Task OnInitializedAsync() {
+        try {
+            this.Role = await SessionStorage.GetItemAsStringAsync(AppRole.Role);
+        } catch {
+        }
+
+        if (!string.IsNullOrEmpty(Role)) {
+            this.User = await this.HttpClientContext.User.GetSingleByIdAsync(this.UserId);
+            if (this.User != null) {
+                this.Roles = await this.HttpClientContext.Role.GetListIsNotDeletedAsync();
+                this.Publishers = await this.HttpClientContext.Publisher.GetListIsNotDeletedAsync();
+                this.User.Role = this.Roles.FirstOrDefault(x => x.Id == this.User.RoleId);
+                this.User.Publisher = this.Publishers.FirstOrDefault(x => x.Id == this.User.PublisherId);
+            }
+        }
+        StateHasChanged();
+    }
+    #endregion
+
+    #region [ Methods - Private ]
+    private async Task CancelAsync() {
+
+        await this.OnInitializedAsync();
+    }
+
+    private async Task UpdateAsync() {
+        var result = await this.HttpClientContext.User.UpdateAsync(this.User);
+        if (result) {
+            await this.OnInitializedAsync();
+        }
+
+    }
+
+    private async Task SoftDeleteAsync() {
+        var result = await this.HttpClientContext.User.SoftDeleteAsync(this.User.Id);
+        if (result) {
+            await this.OnInitializedAsync();
+        }
+    }
+
+    private async Task RecoverAsync() {
+        var result = await this.HttpClientContext.User.RecoverAsync(this.User.Id);
+        if (result) {
+            await this.OnInitializedAsync();
+        }
+    }
+    #endregion
+}
---
+++ 06 Apps/Assignment02.BlazorWebApp/Pages/Admin/User/UserListPage.razor.cs
@@ -0,0 +1,56 @@
+﻿using Assignment02.EntityProviders;
+using Assignment02.HttpClientProviders;
+using Blazored.SessionStorage;
+using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment02.BlazorWebApp;
+
+public partial class UserListPage
+{
+    #region [ Properties - Inject]
+    [Inject]
+    private NavigationManager Navigation { get; set; }
+
+    [Inject]
+    private ISessionStorageService SessionStorage { get; set; }
+
+    [Inject]
+    private HttpClientContext HttpClientContext { get; set; }
+    #endregion
+
+    #region [ Properties ]
+    public IEnumerable<User> Users { get; set; }
+    public IEnumerable<Role> Roles{ get; set; }
+    public IEnumerable<Publisher> Publishers { get; set; }
+    private string Role { get; set; }
+    #endregion
+
+    #region [ Methods - Override ]
+    protected override async Task OnInitializedAsync() {
+        try {
+            this.Role = await SessionStorage.GetItemAsStringAsync(AppRole.Role);
+        } catch {
+        }
+
+        if (!string.IsNullOrEmpty(Role)) {
+            this.Users = await this.HttpClientContext.User.GetListAllAsync();
+            this.Roles = await this.HttpClientContext.Role.GetListAllAsync();
+            this.Publishers = await this.HttpClientContext.Publisher.GetListAllAsync();
+            foreach (var item in Users) {
+                item.Role = this.Roles.FirstOrDefault(x => x.Id == item.RoleId);
+                item.Publisher = this.Publishers.FirstOrDefault(x => x.Id == item.PublisherId);
+            }
+        }
+        StateHasChanged();
+    }
+    #endregion
+
+    #region [ Private Methods -  ]
+    private void ViewUserDetailPage(string userId) {
+        this.Navigation.NavigateTo($"/Admin/Users/Details/{userId}");
+    }
+    #endregion
+}
